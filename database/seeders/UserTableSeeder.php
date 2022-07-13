c346246ca97f8258bab17851dc6caf05276ce717
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-07-13 08:54:01 +0700

    feat: setup spatie

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// add User Model
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@moonton.test',
+            'password' => bcrypt('password')
+        ]);
+
+        $admin->assignRole('admin');
+    }
+}
